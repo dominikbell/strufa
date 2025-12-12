19ec4b0bb02109bbaac132efe5f3c6df09d9ee66
Dominik Bell <dominik.bell@ipp.mpg.de>
2025-12-12 14:10:56 +0100

    forgot to commit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
-#include<optional>
-#include<iostream>
+#include <iostream>
+#include <optional>
 
-#include "io/iohandler.hpp"
-#include "io/parameters/parameters.hpp"
+#include "io/input_parser.hpp"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   if (argc > 1) {
     std::cout << "Program was called with parameter " << argv[1] << '\n';
 
-    // need to use std::strlen because sizeof gives size of pointer
-    std::string filename { static_cast<std::string>(argv[1]) };
-    const int filename_length { static_cast<int>(filename.length()) };
-    std::optional<Parameters> parameters {readParameterFile(filename, filename_length)};
+    const Input input = parse_input(argv);
+    std::cout << "The model name is " << input.model_name << '\n';
 
-    if (parameters) {
-      std::cout << "Succesfully read in parameters! The models name is: " << (*parameters).model_name << '\n';
-    } else {
-      std::cout << "Something went wrong while reading in parameters!\n";
-    }
-
+    /** PSEUDO CODE
+     * const Input input = parser(argv[1])
+     *
+     * switch (input.model_name) {
+     * case "Advection": {
+     *  run_advection(input);
+     * }
+     * case "Poisson": {
+     *  run_poisson(input);
+     * }
+     * }
+     */
   } else {
     std::cout << "Program was called without additional parameters.\n";
   }
